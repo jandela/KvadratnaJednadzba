d1132897d00a6b89d34782fc1b15863169f6e05c
jandela <andjela.cirjak@gmail.com>
2021-11-11 18:35:28 +0100

    Added logging

--- GUI/Program.cs
+++ GUI/Program.cs
@@ -4,19 +4,25 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+[assembly: log4net.Config.XmlConfigurator(Watch = true)]
+
 namespace Vsite.CSharp.KvadratnaJednadzba.Gui
 {
     static class Program
     {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger
+        (System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            log.Info("Program is starting.");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            log.Info("Program is ending.");
         }
     }
 }
